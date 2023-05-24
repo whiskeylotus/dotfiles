c10275b31fde733ac87f254256186e17399470a2
whiskeylotus <0xpentest@proton.me>
2023-05-24 15:21:22 +0200

    create .cache/vim directory at setup

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+mkdir -p ~/.cache/vim
 
 echo "source ~/dotfiles/aliases.zsh" >> ~/.zshrc
